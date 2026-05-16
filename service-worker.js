04cc4209662b32ea475c87872afc4a3e528cff05
svhkvolley <rica.panter@gmail.com>
2026-05-16 10:41:28 +0200

    Fix icon paths in service worker cache

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ const ASSETS = [
   './index.html',
   './manifest.json',
   './service-worker.js',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
